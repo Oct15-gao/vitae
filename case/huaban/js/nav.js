67e991c23f00300d31b097daef7f88501be8b9a6
Oct15-gao <32303964+Oct15-gao@users.noreply.github.com>
2017-10-16 13:52:40 +0800

    Revert "add res"
    This reverts commit 9042cc5d9d906e6ff8f7eac9d6b29a58d1cc3188.

--- case/huaban/js/nav.js
+++ case/huaban/js/nav.js
@@ -1,19 +1,14 @@
 $(document).ready(function() {
     $(window).scroll(function(event) {
     var hei = $(window).scrollTop();
-    if (hei >= 250) {
-        $('.bgWhiteNav').fadeIn()
-        $('.fixedNav').fadeOut();
+    if (hei >= 130) {
+        $('.fixedNav').addClass('bgWhite')
+        $('.fixedNav .bgWhiteSearch').show()
     }else {
-        $('.fixedNav').fadeIn();
-        $('.bgWhiteNav').fadeOut()
+        $('.fixedNav').removeClass('bgWhite')
+        $('.fixedNav .bgWhiteSearch').hide()
     }
+
     });
-    $('.registerPage').hide()
-    $('.regi').click(function(event) {
-        $('.registerPage').show()
-    });
-    $('.remove').click(function(event) {
-        $('.registerPage').hide()
-    });
+
 });
