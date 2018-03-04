3d50c0e94c0b422f19331a93fcd1f96a42377f12
Oct15-gao <32303964+Oct15-gao@users.noreply.github.com>
2018-03-04 11:16:00 +0800

    2018-3-4

--- js/vitae.js
+++ js/vitae.js
@@ -1,24 +1,10 @@
 setTimeout(function(){
-    $("#loading").slideUp(5000)
-},3000)
+    $("#loading").slideUp(1500)
+},0)
 $(document).ready(function() {
 
-    $('#works').hide()
-    $('#speak').hide()
     $('.menu li').click(function(event) {
     $(this).addClass('active').siblings().removeClass('active')
-    });
-
-    // 切换页面
-    $('.about').click(function(event) {
-    $('#index').slideDown(500).siblings('section').slideUp(30)
-    });
-
-    $('.portfolio').click(function(event) {
-    $('#works').slideDown(500).siblings('section').slideUp(30)
-    });
-    $('.speak').click(function(event) {
-    $('#speak').slideDown(500).siblings('section').slideUp(30)
     });
 
 
